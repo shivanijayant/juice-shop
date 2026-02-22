1f6932d871f5d6385bd9fecd1527d16f2df53f23
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2026-02-22 22:57:18 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- routes/search.ts
+++ routes/search.ts
@@ -21,7 +21,7 @@ export function searchProducts () {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     models.sequelize.query(
-      `SELECT * FROM Products WHERE ((name LIKE '%:criteria%' OR description LIKE '%:criteria%') AND deletedAt IS NULL) ORDER BY name`,
+      'SELECT * FROM Products WHERE ((name LIKE \'%:criteria%\' OR description LIKE \'%:criteria%\') AND deletedAt IS NULL) ORDER BY name',
       { replacements: { criteria } }
     ) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
